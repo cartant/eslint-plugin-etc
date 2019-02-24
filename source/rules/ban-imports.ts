ff07b35be653d9e8a121565025276fb3fdb08d34
Nicholas Jamieson <nicholas@cartant.com>
2019-02-24 12:51:05 +1000

    feat: Implement ban-imports.

---
+++ source/rules/ban-imports.ts
@@ -0,0 +1,47 @@
+/**
+ * @license Use of this source code is governed by an MIT-style license that
+ * can be found in the LICENSE file at https://github.com/cartant/eslint-plugin-etc
+ */
+/* tslint:disable:object-literal-sort-keys */
+
+import { Rule } from "eslint";
+import * as ESTree from "estree";
+
+const rule: Rule.RuleModule = {
+    meta: {
+        docs: {
+            category: "General",
+            description: "Forbids using the configured import locations.",
+            recommended: false
+        },
+        fixable: null,
+        messages: {
+            forbidden: "This import location is forbidden."
+        },
+        schema: [{
+            type: "object"
+        }]
+    },
+    create: context => {
+        const [config = {}] = context.options;
+        const forbiddens: RegExp[] = [];
+        Object.entries(config).forEach(([key, value]) => {
+            if (value !== false) {
+                forbiddens.push(new RegExp(key));
+            }
+        });
+        return {
+            ImportDeclaration: (node: ESTree.ImportDeclaration) => {
+                const { source } = node;
+                if (forbiddens.some(forbidden => forbidden.test(source.value as string))) {
+                    context.report({
+                        messageId: "forbidden",
+                        node
+                    });
+                }
+            }
+        };
+    }
+};
+
+export = rule;
---
+++ tests/rules/ban-imports.ts
@@ -0,0 +1,43 @@
+/**
+ * @license Use of this source code is governed by an MIT-style license that
+ * can be found in the LICENSE file at https://github.com/cartant/eslint-plugin-etc
+ */
+/* tslint:disable:object-literal-sort-keys */
+
+import { RuleTester } from "eslint";
+import rule = require("../../source/rules/ban-imports");
+
+const ruleTester = new RuleTester({
+    parserOptions: {
+        ecmaVersion: 6,
+        sourceType: "module"
+    }
+});
+const options = [{
+    "^a$": true,
+    "^b$": false,
+    "(^|/)c$": true
+}];
+
+ruleTester.run("ban-imports", rule, {
+    valid: [{
+        code: `import { b } from "b";`,
+        options
+    }, {
+        code: `import { d } from "./d";`,
+        options
+    }],
+    invalid: [{
+        code: `import { a } from "a";`,
+        errors: [{
+            messageId: "forbidden"
+        }],
+        options
+    }, {
+        code: `import { c } from "./c";`,
+        errors: [{
+            messageId: "forbidden"
+        }],
+        options
+    }]
+});
