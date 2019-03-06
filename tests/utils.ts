adeee81b3a5faaf26bda2274838d705ccd387d45
Nicholas Jamieson <nicholas@cartant.com>
2019-03-07 07:00:24 +1000

    chore: Add testing utils.

---
+++ tests/utils.ts
@@ -0,0 +1,21 @@
+/**
+ * @license Use of this source code is governed by an MIT-style license that
+ * can be found in the LICENSE file at https://github.com/cartant/eslint-plugin-etc
+ */
+
+import path = require("path");
+
+export const configWithoutTypes = {
+    parser: "@typescript-eslint/parser"
+};
+
+export const configWithTypes = {
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        project: path.join(__dirname, "./tsconfig.json")
+    }
+};
+
+export function lines(...lines: string[]): string {
+    return lines.join("\n");
+}
