26832f9da4c14cd73246a62449230e306037bddf
Nicholas Jamieson <nicholas@cartant.com>
2019-09-10 08:16:04 +1000

    refactor: Use createRuleTester.

--- tests/utils.ts
+++ tests/utils.ts
@@ -3,35 +3,6 @@
  * can be found in the LICENSE file at https://github.com/cartant/eslint-plugin-etc
  */
 
-import { RuleTester } from "eslint";
-import { join } from "path";
+import { createRuleTester } from "eslint-etc";
 
-export function ruleTester({
-  comments = false,
-  types
-}: {
-  comments?: boolean;
-  types: boolean;
-}) {
-  const filename = join(__dirname, "file.ts");
-  const tester = new RuleTester({
-    parser: join(__dirname, "../node_modules/@typescript-eslint/parser"),
-    parserOptions: {
-      comments,
-      ecmaVersion: 6,
-      project: types ? join(__dirname, "./tsconfig.json") : undefined,
-      sourceType: "module"
-    }
-  });
-  const run = tester.run;
-  tester.run = (name, rule, { invalid = [], valid = [] }) =>
-    run.call(tester, name, rule, {
-      invalid: invalid.map(test => ({ ...test, filename })),
-      valid: valid.map(test =>
-        typeof test === "string"
-          ? { code: test, filename }
-          : { ...test, filename }
-      )
-    });
-  return tester;
-}
+export const ruleTester = createRuleTester();
