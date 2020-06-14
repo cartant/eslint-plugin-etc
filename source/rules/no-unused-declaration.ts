66b71cb2680e5eac89730e173988280ef823f0a6
Nicholas Jamieson <nicholas@cartant.com>
2020-06-14 22:15:55 +1000

    refactor: Rearrange no-unused-declaration.

--- source/rules/no-unused-declaration.ts
+++ source/rules/no-unused-declaration.ts
@@ -72,170 +72,7 @@ const rule = ruleCreator({
       )
     );
 
-    const isAssignee = (identifier: es.Identifier) => {
-      const parent = getParent(identifier);
-      return (
-        parent.type === "AssignmentExpression" && identifier === parent.left
-      );
-    };
-
-    const isDeclared = (node: es.Node) => {
-      let parent = getParent(node);
-      while (parent) {
-        switch (parent.type) {
-          case "ClassDeclaration":
-          case "TSDeclareFunction":
-          case "VariableDeclaration":
-            return parent.declare;
-          default:
-            break;
-        }
-        parent = getParent(parent);
-      }
-      return false;
-    };
-
-    const isExported = (node: es.Node) => {
-      let parent = getParent(node);
-      while (parent) {
-        if (parent.type === "ExportNamedDeclaration") {
-          return true;
-        }
-        if (isDeclaration(parent)) {
-          return false;
-        }
-        parent = getParent(parent);
-      }
-      return false;
-    };
-
-    const getImportSpecifier = (node: es.Node) => {
-      const parent = getParent(node);
-      switch (parent.type) {
-        case "ImportDefaultSpecifier":
-        case "ImportNamespaceSpecifier":
-        case "ImportSpecifier":
-          return parent;
-        default:
-          return undefined;
-      }
-    };
-
-    const isImported = (node: es.Node) => {
-      const specifier = getImportSpecifier(node);
-      return specifier !== undefined;
-    };
-
-    const isFunctionExpressionIdentifier = (identifier: es.Identifier) => {
-      const parent = getParent(identifier);
-      return parent.type === "FunctionExpression";
-    };
-
-    const isParameter = (node: es.Node): boolean => {
-      const parent = getParent(node);
-      switch (parent.type) {
-        case "ArrayPattern":
-        case "ObjectPattern":
-          return isParameter(parent);
-        case "ArrowFunctionExpression":
-        case "FunctionDeclaration":
-        case "FunctionExpression":
-        case "TSDeclareFunction":
-          return parent.params.includes(node as es.Identifier);
-        case "Property":
-          return isParameter(getParent(parent));
-        case "RestElement":
-          return isParameter(parent);
-        case "TSParameterProperty":
-          return true;
-        default:
-          return false;
-      }
-    };
-
-    const isDeclaration = (node: es.Node): boolean => {
-      const parent = getParent(node);
-      if (!parent) {
-        return false;
-      }
-      switch (parent.type) {
-        case "ArrayPattern":
-        case "ObjectPattern":
-          return isDeclaration(parent);
-        case "ArrowFunctionExpression":
-          return node !== parent.body;
-        case "ClassDeclaration":
-        case "FunctionDeclaration":
-          return true;
-        case "VariableDeclarator":
-          return node !== parent.init;
-        case "Property":
-          return isDeclaration(getParent(parent));
-        case "RestElement":
-          return isDeclaration(parent);
-        default:
-          return isParameter(node);
-      }
-    };
-
-    const isRestElementSibling = (variable: eslint.Scope.Variable) => {
-      const [def] = variable.defs;
-      if (!def) {
-        return false;
-      }
-      const { node } = def;
-      if (!isVariableDeclarator(node) || !isObjectPattern(node.id)) {
-        return false;
-      }
-      return node.id.properties.some(
-        (property) =>
-          isRestElement(property) &&
-          isIdentifier(property.argument) &&
-          property.argument.name !== variable.name
-      );
-    };
-
-    const isTSParameterProperty = (identifier: es.Identifier) => {
-      const parent = getParent(identifier);
-      return parent && parent.type === "TSParameterProperty";
-    };
-
-    const shouldCheckReferences = (identifier: es.Identifier) => {
-      if (!declarations && !isImported(identifier)) {
-        return false;
-      }
-      if (!imports && isImported(identifier)) {
-        return false;
-      }
-      if (isExported(identifier) || isDeclared(identifier)) {
-        return false;
-      }
-      if (ignoredRegExps.some((regExp) => regExp.test(identifier.name))) {
-        return false;
-      }
-      if (isParameter(identifier)) {
-        return false;
-      }
-      if (isTSParameterProperty(identifier)) {
-        return false;
-      }
-      if (isFunctionExpressionIdentifier(identifier)) {
-        return false;
-      }
-      return true;
-    };
-
-    const shouldCountReference = (identifier: es.Identifier) => {
-      if (isDeclaration(identifier)) {
-        return false;
-      }
-      if (isAssignee(identifier)) {
-        return false;
-      }
-      return true;
-    };
-
-    const check = (scope: eslint.Scope.Scope) => {
+    function check(scope: eslint.Scope.Scope) {
       const type: string = scope.type;
       if (type === "enum") {
         return;
@@ -289,9 +126,9 @@ const rule = ruleCreator({
         });
       }
       scope.childScopes.forEach(check);
-    };
+    }
 
-    const checkTypeDeclaration = (node: es.Node & { id: es.Identifier }) => {
+    function checkTypeDeclaration(node: es.Node & { id: es.Identifier }) {
       const typeDeclaration = esTreeNodeToTSNodeMap.get(node) as
         | ts.InterfaceDeclaration
         | ts.TypeAliasDeclaration;
@@ -310,7 +147,165 @@ const rule = ruleCreator({
           node: node.id,
         });
       }
-    };
+    }
+
+    function getImportSpecifier(node: es.Node) {
+      const parent = getParent(node);
+      switch (parent.type) {
+        case "ImportDefaultSpecifier":
+        case "ImportNamespaceSpecifier":
+        case "ImportSpecifier":
+          return parent;
+        default:
+          return undefined;
+      }
+    }
+
+    function isAssigneeIdentifier(identifier: es.Identifier) {
+      const parent = getParent(identifier);
+      return (
+        parent.type === "AssignmentExpression" && identifier === parent.left
+      );
+    }
+
+    function isDeclarationIdentifier(identifier: es.Node): boolean {
+      const parent = getParent(identifier);
+      if (!parent) {
+        return false;
+      }
+      switch (parent.type) {
+        case "ArrayPattern":
+        case "ObjectPattern":
+          return isDeclarationIdentifier(parent);
+        case "ArrowFunctionExpression":
+          return identifier !== parent.body;
+        case "ClassDeclaration":
+        case "FunctionDeclaration":
+          return true;
+        case "VariableDeclarator":
+          return identifier !== parent.init;
+        case "Property":
+          return isDeclarationIdentifier(getParent(parent));
+        case "RestElement":
+          return isDeclarationIdentifier(parent);
+        default:
+          return isParameterIdentifier(identifier);
+      }
+    }
+
+    function isDeclaredIdentifier(identifier: es.Node) {
+      let parent = getParent(identifier);
+      while (parent) {
+        switch (parent.type) {
+          case "ClassDeclaration":
+          case "TSDeclareFunction":
+          case "VariableDeclaration":
+            return parent.declare;
+          default:
+            break;
+        }
+        parent = getParent(parent);
+      }
+      return false;
+    }
+
+    function isExportedIdentifier(identifier: es.Node) {
+      let parent = getParent(identifier);
+      while (parent) {
+        if (parent.type === "ExportNamedDeclaration") {
+          return true;
+        }
+        if (isDeclarationIdentifier(parent)) {
+          return false;
+        }
+        parent = getParent(parent);
+      }
+      return false;
+    }
+
+    function isImportedIdentifier(identifier: es.Node) {
+      const specifier = getImportSpecifier(identifier);
+      return specifier !== undefined;
+    }
+
+    function isFunctionExpressionIdentifier(identifier: es.Identifier) {
+      const parent = getParent(identifier);
+      return parent.type === "FunctionExpression";
+    }
+
+    function isParameterIdentifier(identifier: es.Node): boolean {
+      const parent = getParent(identifier);
+      switch (parent.type) {
+        case "ArrayPattern":
+        case "ObjectPattern":
+          return isParameterIdentifier(parent);
+        case "ArrowFunctionExpression":
+        case "FunctionDeclaration":
+        case "FunctionExpression":
+        case "TSDeclareFunction":
+          return parent.params.includes(identifier as es.Identifier);
+        case "Property":
+          return isParameterIdentifier(getParent(parent));
+        case "RestElement":
+          return isParameterIdentifier(parent);
+        case "TSParameterProperty":
+          return true;
+        default:
+          return false;
+      }
+    }
+
+    function isRestElementSibling(variable: eslint.Scope.Variable) {
+      const [def] = variable.defs;
+      if (!def) {
+        return false;
+      }
+      const { node } = def;
+      if (!isVariableDeclarator(node) || !isObjectPattern(node.id)) {
+        return false;
+      }
+      return node.id.properties.some(
+        (property) =>
+          isRestElement(property) &&
+          isIdentifier(property.argument) &&
+          property.argument.name !== variable.name
+      );
+    }
+
+    function shouldCheckReferences(identifier: es.Identifier) {
+      if (!declarations && !isImportedIdentifier(identifier)) {
+        return false;
+      }
+      if (!imports && isImportedIdentifier(identifier)) {
+        return false;
+      }
+      if (
+        isExportedIdentifier(identifier) ||
+        isDeclaredIdentifier(identifier)
+      ) {
+        return false;
+      }
+      if (ignoredRegExps.some((regExp) => regExp.test(identifier.name))) {
+        return false;
+      }
+      if (isParameterIdentifier(identifier)) {
+        return false;
+      }
+      if (isFunctionExpressionIdentifier(identifier)) {
+        return false;
+      }
+      return true;
+    }
+
+    function shouldCountReference(identifier: es.Identifier) {
+      if (isDeclarationIdentifier(identifier)) {
+        return false;
+      }
+      if (isAssigneeIdentifier(identifier)) {
+        return false;
+      }
+      return true;
+    }
 
     return {
       JSXOpeningElement: (node: es.Node) => {},
