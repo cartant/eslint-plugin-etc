1d820ad3428c52977bbed5b58034ec87ef226d08
Nicholas Jamieson <nicholas@cartant.com>
2021-04-14 22:25:43 +1000

    feat: Implement more of underscore-internal.

--- source/rules/underscore-internal.ts
+++ source/rules/underscore-internal.ts
@@ -4,7 +4,8 @@
  */
 
 import { TSESTree as es } from "@typescript-eslint/experimental-utils";
-import { findParent, getParserServices } from "eslint-etc";
+import { findParent, getParserServices, isIdentifier } from "eslint-etc";
+import * as ts from "typescript";
 import { getTagsFromDeclaration } from "../tslint-tag";
 import { ruleCreator } from "../utils";
 
@@ -27,18 +28,77 @@ const rule = ruleCreator({
   name: "underscore-internal",
   create: (context) => {
     const { esTreeNodeToTSNodeMap } = getParserServices(context);
+
+    function checkDeclaration(identifier: es.BindingName, tsNode: ts.Node) {
+      const tags = getTagsFromDeclaration("internal", tsNode);
+      if (tags.length > 0) {
+        context.report({
+          messageId: "forbidden",
+          node: identifier,
+        });
+      }
+    }
+
     return {
-      "VariableDeclarator[id.name=/^[^_]/]": (node: es.VariableDeclarator) => {
-        const tsNode = esTreeNodeToTSNodeMap.get(
-          findParent(node, "VariableDeclaration") as es.VariableDeclaration
-        );
-        const tags = getTagsFromDeclaration("internal", tsNode);
-        if (tags.length > 0) {
-          context.report({
-            messageId: "forbidden",
-            node: node.id,
-          });
+      "ClassDeclaration[id.name=/^[^_]/]": (node: es.ClassDeclaration) => {
+        if (node.id) {
+          checkDeclaration(node.id, esTreeNodeToTSNodeMap.get(node));
         }
+      },
+      "ClassProperty[key.name=/^[^_]/]": (node: es.ClassProperty) => {
+        if (isIdentifier(node.key)) {
+          checkDeclaration(node.key, esTreeNodeToTSNodeMap.get(node));
+        }
+      },
+      "FunctionDeclaration[id.name=/^[^_]/]": (
+        node: es.FunctionDeclaration
+      ) => {
+        if (node.id) {
+          checkDeclaration(node.id, esTreeNodeToTSNodeMap.get(node));
+        }
+      },
+      "MethodDefinition[key.name=/^[^_]/]": (node: es.MethodDefinition) => {
+        if (isIdentifier(node.key)) {
+          checkDeclaration(node.key, esTreeNodeToTSNodeMap.get(node));
+        }
+      },
+      "TSEnumDeclaration[id.name=/^[^_]/]": (node: es.TSEnumDeclaration) => {
+        checkDeclaration(node.id, esTreeNodeToTSNodeMap.get(node));
+      },
+      "TSEnumMember[id.name=/^[^_]/]": (node: es.TSEnumMember) => {
+        if (isIdentifier(node.id)) {
+          checkDeclaration(node.id, esTreeNodeToTSNodeMap.get(node));
+        }
+      },
+      "TSInterfaceDeclaration[id.name=/^[^_]/]": (
+        node: es.TSInterfaceDeclaration
+      ) => {
+        checkDeclaration(node.id, esTreeNodeToTSNodeMap.get(node));
+      },
+      "TSMethodSignature[key.name=/^[^_]/]": (node: es.TSMethodSignature) => {
+        if (isIdentifier(node.key)) {
+          checkDeclaration(node.key, esTreeNodeToTSNodeMap.get(node));
+        }
+      },
+      "TSPropertySignature[key.name=/^[^_]/]": (
+        node: es.TSPropertySignature
+      ) => {
+        if (isIdentifier(node.key)) {
+          checkDeclaration(node.key, esTreeNodeToTSNodeMap.get(node));
+        }
+      },
+      "TSTypeAliasDeclaration[id.name=/^[^_]/]": (
+        node: es.TSTypeAliasDeclaration
+      ) => {
+        checkDeclaration(node.id, esTreeNodeToTSNodeMap.get(node));
+      },
+      "VariableDeclarator[id.name=/^[^_]/]": (node: es.VariableDeclarator) => {
+        checkDeclaration(
+          node.id,
+          esTreeNodeToTSNodeMap.get(
+            findParent(node, "VariableDeclaration") as es.VariableDeclaration
+          )
+        );
       },
     };
   },
--- tests/rules/underscore-internal.ts
+++ tests/rules/underscore-internal.ts
@@ -13,6 +13,65 @@ ruleTester({ types: true }).run("underscore-internal", rule, {
     {
       code: stripIndent`
         /**
+         * Some class
+         */
+        export class SomeClass {}
+      `,
+    },
+    {
+      code: stripIndent`
+        /**
+         * Some internal class with _
+         * @internal
+         */
+        export class _SomeClass {}
+      `,
+    },
+    {
+      code: stripIndent`
+        export class SomeClass {
+          /**
+           * Some class method
+           */
+          someMethod() {}
+        }
+      `,
+    },
+    {
+      code: stripIndent`
+        export class SomeClass {
+          /**
+           * Some internal class method with _
+           * @internal
+           */
+          _someMethod() {}
+        }
+      `,
+    },
+    {
+      code: stripIndent`
+        export class SomeClass {
+          /**
+           * Some class property
+           */
+          someProperty: unknown;
+        }
+      `,
+    },
+    {
+      code: stripIndent`
+        export class SomeClass {
+          /**
+           * Some internal class property with _
+           * @internal
+           */
+          _someProperty: unknown;
+        }
+      `,
+    },
+    {
+      code: stripIndent`
+        /**
          * Some constant
          */
         export const SOME_CONSTANT = 0;
@@ -27,6 +86,137 @@ ruleTester({ types: true }).run("underscore-internal", rule, {
         export const _SOME_CONSTANT = 0;
       `,
     },
+    {
+      code: stripIndent`
+        /**
+         * Some enum
+         */
+        export enum SomeEnum {}
+      `,
+    },
+    {
+      code: stripIndent`
+        /**
+         * Some internal enum with _
+         * @internal
+         */
+        export enum _SomeEnum {}
+      `,
+    },
+    {
+      code: stripIndent`
+        export enum SomeEnum {
+          /**
+           * Some enum member
+           */
+          SOME_MEMBER = 0
+        }
+      `,
+    },
+    {
+      code: stripIndent`
+        export enum _SomeEnum {
+          /**
+           * Some internal enum member with _
+           * @internal
+           */
+          _SOME_MEMBER = 0
+        }
+      `,
+    },
+    {
+      code: stripIndent`
+        /**
+         * Some function
+         */
+        export function someFunction() {}
+      `,
+    },
+    {
+      code: stripIndent`
+        /**
+         * Some internal function with _
+         * @internal
+         */
+        export function _someFunction() {}
+      `,
+    },
+    {
+      code: stripIndent`
+        /**
+         * Some interface
+         */
+        export interface SomeInterface {}
+      `,
+    },
+    {
+      code: stripIndent`
+        /**
+         * Some internal interface with _
+         * @internal
+         */
+        export interface _SomeInterface {}
+      `,
+    },
+    {
+      code: stripIndent`
+        export interface SomeInterface {
+          /**
+           * Some method
+           */
+          someMethod(): unknown;
+        }
+      `,
+    },
+    {
+      code: stripIndent`
+        export interface SomeInterface {
+          /**
+           * Some internal method with _
+           * @internal
+           */
+          _someMethod(): unknown;
+        }
+      `,
+    },
+    {
+      code: stripIndent`
+        export interface SomeInterface {
+          /**
+           * Some interface
+           */
+          someProperty: unknown;
+        }
+      `,
+    },
+    {
+      code: stripIndent`
+        export interface SomeInterface {
+          /**
+           * Some internal interface with _
+           * @internal
+           */
+          _someProperty: unknown;
+        }
+      `,
+    },
+    {
+      code: stripIndent`
+        /**
+         * Some type alias
+         */
+        export type SomeType = {};
+      `,
+    },
+    {
+      code: stripIndent`
+        /**
+         * Some internal type alias with _
+         * @internal
+         */
+        export type _SomeType = {};
+      `,
+    },
   ],
   invalid: [
     fromFixture(
@@ -37,6 +227,116 @@ ruleTester({ types: true }).run("underscore-internal", rule, {
          */
         export const SOME_CONSTANT = 0;
                      ~~~~~~~~~~~~~ [forbidden]
+      `
+    ),
+    fromFixture(
+      stripIndent`
+        /**
+         * Some internal class without _
+         * @internal
+         */
+        export class SomeClass {}
+                     ~~~~~~~~~ [forbidden]
+      `
+    ),
+    fromFixture(
+      stripIndent`
+        export class SomeClass {
+          /**
+           * Some internal class method without _
+           * @internal
+           */
+          someMethod() {}
+          ~~~~~~~~~~ [forbidden]
+        }
+      `
+    ),
+    fromFixture(
+      stripIndent`
+        export class SomeClass {
+          /**
+           * Some internal class property without _
+           * @internal
+           */
+          someProperty: unknown;
+          ~~~~~~~~~~~~ [forbidden]
+        }
+      `
+    ),
+    fromFixture(
+      stripIndent`
+        /**
+         * Some internal function without _
+         * @internal
+         */
+        export function someFunction() {}
+                        ~~~~~~~~~~~~ [forbidden]
+      `
+    ),
+    fromFixture(
+      stripIndent`
+        /**
+         * Some internal enum without _
+         * @internal
+         */
+        export enum SomeEnum {}
+                    ~~~~~~~~ [forbidden]
+      `
+    ),
+    fromFixture(
+      stripIndent`
+        export enum SomeEnum {
+          /**
+           * Some internal enum member without _
+           * @internal
+           */
+          SOME_MEMBER = 0
+          ~~~~~~~~~~~ [forbidden]
+        }
+      `
+    ),
+    fromFixture(
+      stripIndent`
+        /**
+         * Some internal interface without _
+         * @internal
+         */
+        export interface SomeInterface {}
+                         ~~~~~~~~~~~~~ [forbidden]
+      `
+    ),
+    fromFixture(
+      stripIndent`
+        export interface SomeInterface {
+          /**
+           * Some internal method without _
+           * @internal
+           */
+          someMethod(): unknown;
+          ~~~~~~~~~~ [forbidden]
+        }
+      `
+    ),
+    fromFixture(
+      stripIndent`
+        export interface SomeInterface {
+          /**
+           * Some internal property without _
+           * @internal
+           */
+          someProperty: unknown;
+          ~~~~~~~~~~~~ [forbidden]
+        }
+      `
+    ),
+    fromFixture(
+      stripIndent`
+        /**
+         * Some internal type alias without _
+         * @internal
+         */
+        export type SomeType = {};
+                    ~~~~~~~~ [forbidden]
       `
     ),
   ],
